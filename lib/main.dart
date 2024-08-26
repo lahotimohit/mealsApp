bd8e1339e16dae7c4eb06417ad135d79b10e0010
Mohit Lahoti <engtechno25@gmail.com>
2024-08-26 19:54:43 +0530

    created categroy model and add dummy category also add these model to grid item

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mealsapp/categories.dart';
+import 'package:mealsapp/screens/categories.dart';
 
 final theme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
