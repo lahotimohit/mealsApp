bcfda6f435333450ae3816250f5daa7db898f450
Mohit Lahoti <engtechno25@gmail.com>
2024-08-27 23:45:18 +0530

    Adding images in meals category screen with stack widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp/models/meal.dart';
+import 'package:mealsapp/widgets/meals.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meal});
@@ -11,7 +12,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
         itemCount: meal.length,
-        itemBuilder: (context, index) => Text(meal[index].title));
+        itemBuilder: (context, index) => Meals(meal: meal[index]));
 
     if (meal.isEmpty) {
       content = Center(
