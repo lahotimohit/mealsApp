fbd49fec713be4ad28b144e88f742b112c79828f
Mohit Lahoti <engtechno25@gmail.com>
2024-08-29 00:28:06 +0530

    Improving meal details screen by adding ingredients and steps on screen

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -11,11 +11,54 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(mealItem.title),
       ),
-      body: Image.network(
-        mealItem.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              mealItem.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14),
+            Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            for (final ingredient in mealItem.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+            const SizedBox(height: 12),
+            Text(
+              "Steps",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            for (final step in mealItem.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Theme.of(context).colorScheme.onSurface),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
