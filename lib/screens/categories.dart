35d6d16ec739889c335d1961dda725a0850375e6
Mohit Lahoti <engtechno25@gmail.com>
2024-11-29 02:03:02 +0530

    slide transition with drive method of animation controller

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -70,9 +70,13 @@ class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerPr
           )
       ],
     ), 
-    builder: (context, child) => Padding(
-      padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
-      child: child,
-      )) ;
+    builder: (context, child) => SlideTransition(
+      position: _animationController.drive(
+        Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0)
+        )
+      ),
+      child: child,)) ;
   }
 }
